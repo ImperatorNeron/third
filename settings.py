6e083f30350196e13268c685e1105c1496d94617
ImperatorNeron <katebarspk@gmail.com>
2023-07-10 23:08:21 +0300

    fc

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+
+if __name__ == '__main__':
+    print(Settings(2, 3).add())
